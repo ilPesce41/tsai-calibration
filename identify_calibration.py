fe6c646f435860688094eacfc636f7f7aa6bea64
IlPesce41 <coleh@mail.usf.edu>
2020-03-17 08:16:53 -0400

    Wrappers and comments

--- identify_calibration.py
+++ identify_calibration.py
@@ -6,11 +6,13 @@ from multiprocessing import Pool
 from point import Point, newPoint
 from calibration import calibrate
 
+#Real world location of calibration points in cube
 x = 1.15 #units mm
 CAL2REAL = [(-x,x,-2*x),(x,x,-2*x),(-x,-x,-2*x),(x,-x,-2*x),(2*x,x,-x),(2*x,x,x),(2*x,-x,-x),(2*x,-x,x),(x,2*x,-x),(x,2*x,x),(-x,2*x,-x),(-x,2*x,x),
 	(x,-2*x,-x),(x,-2*x,x),(-x,-2*x,-x),(-x,-2*x,x),(x,x,2*x),(-x,x,2*x),(x,-x,2*x),(-x,-x,2*x),(-2*x,x,x),(-2*x,x,-x),(-2*x,-x,x),(-2*x,-x,-x)
 ]
 
+#Point schema for initializing point for tsai algorithm
 pointSchema = [ 'world', 'camera', 'pixel', 'projectedPixel', 'distortedPixel', 'sensor', 'projectedSensor', 'distortedSensor', 'normal', 'projectedNormal', 'distortedNormal' ]
 
 def qr_scan(im):
@@ -28,20 +30,27 @@ def identify_points(filename):
 	points = {}
 	images = []
 
+	#Load image
 	in_im = cv2.imread(filename)
+	#Convert to grayscale
 	in_im = cv2.cvtColor(in_im, cv2.COLOR_BGR2GRAY)
 	im = in_im.copy()
+	#Normalize image color
 	cv2.normalize(in_im,  im, 0, 255, cv2.NORM_MINMAX)
 	images.append(im)
+	#Binary thresholding with multiple thresholds
 	for i in range(10,20):
 		r,binary1 = cv2.threshold(im,i*10,255,cv2.THRESH_BINARY)
 		images.append(binary1.copy())
+	#Binary thresholding with adaptive threshold
 	binary2 = cv2.adaptiveThreshold(im,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,11,2)
 	images.append(binary2)
 
+	#Scan for QR codes in all thresholded images in parallel
 	pool = Pool(20)
 	image_qrs = pool.map(qr_scan,images)
 
+	#Extract QR Code center locations
 	for qrs in image_qrs:
 		for qr in qrs:
 			poly = qr.polygon
@@ -66,6 +75,11 @@ def get_shared(points1, points2):
 	return list(camera_1.intersection(camera_2))
 
 def point_dict_to_points(point_dict):
+	"""
+	Given pixel location of points on calibration object
+	returns point object with real world coordinates for
+	tsai calibration algorithm
+	"""
 	points = []
 	for key,item in point_dict.items():
 		world_coords = CAL2REAL[int(key)-1]
@@ -74,6 +88,31 @@ def point_dict_to_points(point_dict):
 		points.append(point)
 	return points
 
+def tsai_calibration(filename):
+	"""
+	Calibrates camera using Bailus's implementation of
+	Tsai Calibration.
+	"""
+
+	#Extract calibration object points
+	cpoint = identify_points(filename)
+	camera_points = point_dict_to_points(cpoint)
+	params = calibrate(camera_points)
+
+	#Create camera dict for triangulate function
+	camera = {}
+	camera['R'] = params["rotationMatrix"]
+	t = []
+	for v in ['tx','ty','tz']:
+		t.append(params[v])
+	camera['t'] = np.array(t)
+	f = params['f']
+	camera['k'] = np.array([
+			[f,0,0],
+			[0,f,0],
+			[0,0,1]
+		])
+	return camera
 
 if __name__ == "__main__":
 
@@ -85,11 +124,5 @@ if __name__ == "__main__":
 
 	for data_file in data_files:
 		filename = os.path.join(data_dir,data_file)
-		cpoints.append(identify_points(filename))	
-	
-	camera_points = []
-	for cpoint in cpoints:
-		camera_points.append(point_dict_to_points(cpoint))
-	
-	for camera in camera_points:
-		print(calibrate(camera))
+		camera = tsai_calibration(filename)
+		print(camera)
--- triangulate.py
+++ triangulate.py
@@ -2,6 +2,9 @@ import numpy as np
 
 
 def toHomogenous(point):
+	"""
+	Convert point to Homogeneous Coordinates
+	"""
 	dim = len(point)+1
 	lower = np.array(point)
 	higher = np.ones(dim)
@@ -10,29 +13,46 @@ def toHomogenous(point):
 
 
 def triangulate_point(cameras):
-	
+	"""
+	Triangulates the real world location of a point
+	given pixel locations and calibrated camera models
+	"""
+	#Two vectors needed in triangulation calculation
 	c = []
 	v = []
 
+	#Iterate through cameras
 	for camera in cameras:
 
+		#Camera calibration
 		R = camera['R']
 		t = camera['t']
 		K = camera['K']
+
+		#Pixel location in homogeneous coordinates
 		point = toHomogenous(camera['point'])
+
+		#Calculate cj and vj for  point
 		cj = R.T@t
 		V = R.T@np.linalg(K)@point
 		v = V/np.linalg.norm(V)
+
+		#Store cj and vj
 		c.append(c)
 		v.append(v)
 
+	#Matricies to hold two sums
 	A = np.zeros(3,3)
 	B = np.zeros(3)
+
+	#Iterate through (cj,vj) pairs and add
+	#Calculations to A and B
 	for ci,vi in zip([ci,vi]):
 		inter = np.ones((3,3)) - v@v.T
 		A = A + inter
 		B = B + inter@c
 
+	#Triangulate point
 	return np.linalg.inv(A)@B
 
 
